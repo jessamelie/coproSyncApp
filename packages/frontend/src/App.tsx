ffded621d15eecb5ad06fdbfad2891e4da701026
JessAm <jessamelie.sola@gmail.com>
2025-03-22 00:41:37 +0100

    feat: add imbricated routes

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,16 +1,22 @@
-import './App.css'
-import { Routes, Route } from 'react-router-dom'
-import { Dashboard } from './pages/dashboard/Dashboard'
-import { Homepage } from './pages/homepage/Homepage'
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import { Dashboard } from "./pages/dashboard/Dashboard";
+import { Homepage } from "./pages/homepage/Homepage";
+import { Profile } from "./pages/dashboard/profile/Profile";
 
 function App() {
-
   return (
     <Routes>
-     <Route path="/" element={<Homepage/>}/>
-      <Route path="/dashboard" element={<Dashboard/>}/>
+      <Route path="/" element={<Homepage />} />
+      <Route path="/dashboard" element={<Dashboard />}>
+        <Route index element={""} />
+        <Route path="agenda" element={<h1>Agenda</h1>} />
+        <Route path="messages" element={<h1>Messagerie</h1>} />
+        <Route path="copropriete" element={<h1>Copropriété</h1>} />
+      </Route>
+      <Route path="/profile" element={<Profile />} />
     </Routes>
-  )
+  );
 }
 
-export default App
+export default App;
--- packages/frontend/src/pages/dashboard/layout/Layout.tsx
+++ packages/frontend/src/pages/dashboard/layout/Layout.tsx
@@ -1,40 +1,86 @@
 import styles from "./Layout.module.css";
-import { AppShell, Burger, Group, Skeleton, Title } from "@mantine/core";
+import {
+  AppShell,
+  Avatar,
+  Burger,
+  Flex,
+  Group,
+  Title,
+} from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import { useTranslation } from "react-i18next";
+import { IconUser } from "@tabler/icons-react";
+import { Outlet, useNavigate } from "react-router-dom";
 
 export const Layout = () => {
   const { t } = useTranslation();
+  const navigate = useNavigate();
   const [opened, { toggle }] = useDisclosure();
 
   return (
     <AppShell
       header={{ height: { base: 60, md: 70, lg: 80 } }}
       navbar={{
-        width: { base: 200, md: 300, lg: 400 },
+        width: { base: 160, md: 200, lg: 240 },
         breakpoint: "sm",
         collapsed: { mobile: !opened },
       }}
       padding="md"
     >
       <AppShell.Header>
-        <Group h="100%" px="md">
+        <Group h="100%" px="md" style={{ width: "100%" }}>
           <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <Title order={2}>{t("homepage.brand.name.app.title")}</Title>
+          <Flex className={styles.header}>
+            <Title order={2}>{t("homepage.brand.name.app.title")}</Title>
+            <Avatar size="lg">
+              <IconUser
+                className={styles.profileIcon}
+                size={35}
+                onClick={() => navigate("/profile")}
+              />
+            </Avatar>
+          </Flex>
         </Group>
       </AppShell.Header>
 
-      <AppShell.Navbar p="md">
-        Navbar
-        {Array(15)
-          .fill(0)
-          .map((_, index) => (
-            <Skeleton key={index} h={28} mt="sm" animate={false} />
-          ))}
+      <AppShell.Navbar p="sm">
+        <div className={styles.navLinks}>
+          <div>
+            {t("dashboard.side.navbar.general.label")}
+          </div>
+          <div
+            className={styles.navLink}
+            onClick={() => navigate("/dashboard")}
+          >
+            {t("dashboard.side.navbar.dashboard.view.label")}
+          </div>
+          <div
+            className={styles.navLink}
+            onClick={() => navigate("/dashboard/agenda")}
+          >
+            {t("dashboard.side.navbar.agenda.view.label")}
+          </div>
+          <div
+            className={styles.navLink}
+            onClick={() => navigate("/dashboard/messages")}
+          >
+            {t("dashboard.side.navbar.message.view.label")}
+          </div>
+          <div
+            className={styles.navLink}
+            onClick={() => navigate("/dashboard/copropriete")}
+          >
+            {t("dashboard.side.navbar.condominium.view.label")}
+          </div>
+          <div>
+            {t("dashboard.side.navbar.administrative.label")}
+          </div>
+        </div>
       </AppShell.Navbar>
 
-
-      <AppShell.Main>Main</AppShell.Main>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
     </AppShell>
   );
 };
