8217b8bc1c2e3db0e758947ed3c8dbe48f26a6e4
JessAm <jessamelie.sola@gmail.com>
2025-03-21 20:16:52 +0100

    feat: add dashboard page

--- packages/frontend/src/pages/dashboard/Dashboard.tsx
+++ packages/frontend/src/pages/dashboard/Dashboard.tsx
@@ -1,7 +1,9 @@
-export const Dashboard = () => { 
-    return (
-        <div>
-            <h1>Dashboard</h1>
-        </div>
-    )
-}
+import styles from "./Dashboard.module.css";
+
+export const Dashboard = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
